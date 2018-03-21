150d5032a38b7195a09d1b2d39bf7016fb6cd0b5
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-03-21 11:50:03 +0100

    yes

--- odeep/src/main/java/Test.java
+++ odeep/src/main/java/Test.java
@@ -2,13 +2,19 @@ public class Test {
 
     public static void main(String[] args) {
 
-        PeerInformations p1 = new PeerInformations("Lionel", "192.168.0.38", 1234);
-        PeerInformations p2 = new PeerInformations("Jee", "192.168.0.46", 1234);
+        PeerInformations p1 = new PeerInformations("Lionel", "192.168.0.38", 4444);
+        PeerInformations p2 = new PeerInformations("Jee", "192.168.0.46", 4444);
 
         Node n1 = new Node(p1);
         Node n2 = new Node(p2);
 
+        String message1 = "HIHIHIHI";
+        PeerMessage peerMessage1 = new PeerMessage("TEST", message1);
+        String message2 = "HOHOHOHOHO";
+        PeerMessage peerMessage2 = new PeerMessage("TEST", message2);
 
+        n2.sendToPeer(peerMessage1, p1);
+        n2.sendToPeer(peerMessage2, p1);
     }
 
 }
