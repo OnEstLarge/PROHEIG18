20689485ba30bb18d42d0cb98ad2d6442611b484
loic-schurch <loic.schurch@heig-vd.ch>
2018-03-21 10:51:50 +0100

    Implementation de PeerMessage

--- odeep/src/main/java/PeerMessage.java
+++ odeep/src/main/java/PeerMessage.java
@@ -1,14 +1,48 @@
 
 public class PeerMessage {
 
+    private static final int TYPE_LENGTH = 4;
+    private String type;
+    private String message;
 
+    public PeerMessage(String type, String message) {
+        //exception si pas bon format de type
+        if(!isValidTypeFormat(type)) {
+            throw new IllegalArgumentException("Bad type format");
+        }
+        this.type = type;
+        this.message = message;
+    }
+
+    /**
+     * Vérifie que le format du type de message passé en argument est correct.
+     * @param type
+     * @return
+     */
+    public static boolean isValidTypeFormat(String type) {
+        return type != null && type.length() == TYPE_LENGTH && isUpperCase(type);
+    }
+
+    /**
+     * Vérifie si la string passée en argument est composée uniquement de majuscule.
+     * @param s
+     * @return
+     */
+    private static boolean isUpperCase(String s) {
+        for(int i = 0; i < s.length(); ++i) {
+            if(!Character.isUpperCase(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public String getType() {
-        return "";
+        return type;
     }
-
 
     public String getMessage() {
-        return "";
+        return message;
     }
+
 }
