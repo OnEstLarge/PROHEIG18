1a27429d632da7dda3754f3dd28e927410715b17
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-03-22 11:57:52 +0100

    FX

--- FX/src/sample/test.java
+++ FX/src/sample/test.java
@@ -4,23 +4,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
 
-public class Controller implements Initializable {
-    static int groupeID = 0;
+public class test implements Initializable {
 
     @FXML
     private Accordion accordion;
 
     @FXML
-    private void handleCreateButtonAction(ActionEvent event) {
+    private void handleButtonAction(ActionEvent event) {
         AnchorPane newPanelContent = new AnchorPane();
-        newPanelContent.getChildren().add(new Label("Groupe " + groupeID++));
+        newPanelContent.getChildren().add(new Label("Hello World"));
         TitledPane pane = new TitledPane("World Pane", newPanelContent);
         System.out.println(accordion);
         accordion.getPanes().add(pane);
