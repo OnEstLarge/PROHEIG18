a9de3929ff611651be9556ac400dceec26a07f09
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-03-24 13:16:32 +0100

    petite correction PeerHandler

--- odeep/src/main/java/PeerHandler.java
+++ odeep/src/main/java/PeerHandler.java
@@ -32,7 +32,7 @@ public class PeerHandler implements Runnable {
         PeerMessage message = connection.receiveMessage();
 
         //handle message
-        node.getMapMessage().get(message.getType()).handleMessage(message.getMessageContent().toString());
+        node.getMapMessage().get(message.getType()).handleMessage(new String(message.getMessageContent()));
 
         //close connection
         connection.close();
