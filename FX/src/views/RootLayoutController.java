77b991145cdd484c12d1a7e9751e12bec13531a9
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-04-06 14:35:02 +0200

    interface graphique

--- FX/src/views/RootLayoutController.java
+++ FX/src/views/RootLayoutController.java
@@ -1,35 +1,48 @@
-package sample;
+package views;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
-import javafx.scene.layout.AnchorPane;
 
-public class Controller implements Initializable {
+public class RootLayoutController implements Initializable {
+
+    private views.Main mainApp;
     static int groupeID = 1;
+
+    public void setMainApp(views.Main mainApp){
+        this.mainApp = mainApp;
+    }
+
 
     @FXML
     private Accordion accordion;
 
     @FXML
-    private void handleCreateButtonAction(ActionEvent event) {
+    private void handleCreateButtonAction() {
         TitledPane bonjour = new TitledPane();
         ListView view = new ListView();
         bonjour.setText("Groupe " + groupeID++);
         bonjour.setContent(view);
         bonjour.setCollapsible(true);
-
         accordion.getPanes().add(bonjour);
-
     }
 
+    @FXML
+    private void handleInvite(){
+        // TODO: get the groupe in which we add a member
+        String groupe = "";
+        boolean okClicked = mainApp.showInviteDialog(groupe);
+    }
+
+    @FXML
+    private void handleQuit(){
+        System.exit(0);
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
