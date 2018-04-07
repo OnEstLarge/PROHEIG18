61f439eacbd1f9b3372979966b7c649c0fca290d
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-04-07 16:56:45 +0200

    toujours autant relou

--- FX/src/views/InviteDialogController.java
+++ FX/src/views/InviteDialogController.java
@@ -3,10 +3,18 @@ package views;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
+import javafx.scene.control.TextField;
+
 public class InviteDialogController {
 
     private Stage dialogStage;
     private boolean okClicked = false;
+
+    @FXML
+    private TextField groupeIDField;
+
+    @FXML
+    private TextField newUserIPField;
 
     @FXML
     private void initialize() {
@@ -26,9 +34,7 @@ public class InviteDialogController {
      *
      * @return
      */
-    public boolean isOkClicked() {
-        return okClicked;
-    }
+    public boolean isOkClicked() { return okClicked; }
 
     /**
      * Called when the user clicks cancel.
@@ -43,6 +49,8 @@ public class InviteDialogController {
      */
     @FXML
     private void handleOk() {
+        System.out.println(groupeIDField.getText());
+        System.out.println(newUserIPField.getText());
         okClicked = true;
         dialogStage.close();
     }
--- FX/src/views/RootLayoutController.java
+++ FX/src/views/RootLayoutController.java
@@ -1,11 +1,14 @@
 package views;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
 
@@ -13,8 +16,9 @@ public class RootLayoutController implements Initializable {
 
     private views.Main mainApp;
     static int groupeID = 1;
+    static List<TitledPane> listPanes = new ArrayList<>();
 
-    public void setMainApp(views.Main mainApp){
+    public void setMainApp(views.Main mainApp) {
         this.mainApp = mainApp;
     }
 
@@ -24,23 +28,29 @@ public class RootLayoutController implements Initializable {
 
     @FXML
     private void handleCreateButtonAction() {
-        TitledPane bonjour = new TitledPane();
+        TitledPane pane = new TitledPane();
+        listPanes.add(pane);
         ListView view = new ListView();
-        bonjour.setText("Groupe " + groupeID++);
-        bonjour.setContent(view);
-        bonjour.setCollapsible(true);
-        accordion.getPanes().add(bonjour);
+        Label label = new Label();
+        label.setText("Test ip");
+        view.setId("groupe" + groupeID);
+        view.getItems().add(label);
+        listPanes.get(groupeID - 1).setText("Groupe " + groupeID);
+        listPanes.get(groupeID - 1).setContent(view);
+        listPanes.get(groupeID - 1).setCollapsible(true);
+        accordion.getPanes().add(listPanes.get(groupeID - 1));
+        ++groupeID;
     }
 
     @FXML
-    private void handleInvite(){
+    private void handleInvite() {
         // TODO: get the groupe in which we add a member
         String groupe = "";
         boolean okClicked = mainApp.showInviteDialog(groupe);
     }
 
     @FXML
-    private void handleQuit(){
+    private void handleQuit() {
         System.exit(0);
     }
 
