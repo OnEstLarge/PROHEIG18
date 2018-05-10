1fd16297c6bec838a2298f1fb3e70603116f02dd
loic-schurch <loic.schurch@heig-vd.ch>
2018-05-10 14:21:42 +0200

    aaa

--- odeep/src/main/java/Client.java
+++ odeep/src/main/java/Client.java
@@ -1,48 +1,80 @@
+import Node.FileSharingNode;
 import handler.BYEHandler;
 import handler.RSAHandler;
 import handler.SFILHandler;
 import handler.SMESHandler;
 import message.MessageType;
+import peer.PeerInformations;
 import peer.PeerMessage;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class Client {
-    private Socket clientSocket;
-    private BufferedInputStream in;
-    private BufferedOutputStream out;
+    private static final String IP_SERVER = "206.189.49.105";
+    private static final int PORT_SERVER = 8080;
+    private static final int LOCAL_PORT = 4444;
 
-    private ServerSocket serverSocket;
-    private byte cbuf[] = new byte[4096];
-    private byte cbuf1[] = new byte[4096];
+    private static Socket clientSocketToServerPublic;
+    private static BufferedInputStream in;
+    private static BufferedOutputStream out;
+
+    private static FileSharingNode n;
 
     public static void main(String[] args) {
+
+        //Check si première ouverture de l'app
+            //Si première ouverture -> demander pseudo
+
+            //Sinon, on peut le pseudo dans le fichier de config
+
+        //Get local IP used
+        String localIP = null;
+        try {
+            Enumeration e = NetworkInterface.getNetworkInterfaces();
+            while (e.hasMoreElements()) {
+                NetworkInterface n = (NetworkInterface) e.nextElement();
+                Enumeration ee = n.getInetAddresses();
+                while (ee.hasMoreElements()) {
+                    Inet4Address i = null;
+                    try {
+                        i = (Inet4Address) ee.nextElement();
+                        if(!i.getHostAddress().endsWith(".1") && !i.getHostAddress().endsWith(".255")) {
+                            localIP = i.getHostAddress();
+                        }
+                    } catch (ClassCastException ex) {}
+                }
+            }
+        }catch (SocketException e) {}
+
+        System.out.println(localIP);
+
+
+        PeerInformations myInfos = new PeerInformations("MON PSEUDO WESH", localIP, LOCAL_PORT);
+        n = new FileSharingNode(myInfos);
+        //Ajouter tous les handlers
+        //listening for incoming connections
+        n.acceptingConnections();
+
+
+        //connection au server publique
         Client c = new Client();
-        c.initConnections("206.189.49.105", 8080);;
+        c.initConnections(IP_SERVER, PORT_SERVER);
+
     }
 
     //initialise la connection avec le serveur et de lancer le server d'écoute du client
     public void initConnections(String ip, int port) {
         try {
-            clientSocket = new Socket(ip, port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            in = new BufferedInputStream(clientSocket.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            out = new BufferedOutputStream(clientSocket.getOutputStream());
+            clientSocketToServerPublic = new Socket(ip, port);
+            in = new BufferedInputStream(clientSocketToServerPublic.getInputStream());
+            out = new BufferedOutputStream(clientSocketToServerPublic.getOutputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        new Thread(new Server()).start();
         new Thread(new ConnectionServer()).start();
     }
 
@@ -67,33 +99,15 @@ public class Client {
                 while ((read = in.read(buffer)) != -1) {
                     PeerMessage pm = new PeerMessage(buffer);
                     String type = pm.getType();
-                    switch (type) {
 
-                        case MessageType.SFIL:
-                            new SFILHandler().handleMessage();
-                            break;
 
-                        case MessageType.SMES:
-                            new SMESHandler().handleMessage();
-                            break;
-
-                        case MessageType.:
-                            break;
-                        case "BYE":
-                            new BYEHandler().handleMessage();
-                            break;
-
-                        default :
-                            System.out.println("Message pas reconnu");
-
-                    }
                 }
 
             } catch (IOException e) {
                 System.out.println(e.getMessage());
             } finally {
                 try {
-                    clientSocket.close();
+                    clientSocketToServerPublic.close();
                 } catch (IOException e) {
                     System.out.println(e);
                 }
@@ -118,91 +132,6 @@ public class Client {
 
         //TODO faire les trucs de l'inteface graphique ici
         public void run(){
-
-        }
-    }
-
-    //Server qui permet de reçevoir les communications de l'extérieur
-    private class Server implements Runnable {
-        boolean serverStopped = false;
-        ServerSocket serverSocket = null;
-
-        public Server(){
-            try {
-                serverSocket = new ServerSocket(8080);
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-
-        }
-
-        @Override
-        public void run() {
-            while (!serverStopped) {
-                System.out.println("En attente de connection");
-                try {
-                    Socket client = serverSocket.accept();
-                    new Thread(new sendFileClass(client)).start();
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
-    }
-
-    //Permet d'envoyer un fichier diretement vers l'autre personne
-    private class sendFileClass implements Runnable {
-        private Socket socketToSend;
-        private BufferedInputStream inToSend;
-        private BufferedOutputStream outToSend;
-        byte[] buffer = new byte[4096];
-
-        private sendFileClass(Socket toSend) {
-            try {
-                socketToSend = toSend;
-                inToSend = new BufferedInputStream(toSend.getInputStream());
-                outToSend = new BufferedOutputStream(toSend.getOutputStream());
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
-        public void run() {
-            try {
-                //on lit le nom du fichier voulu
-                int read = inToSend.read(buffer);
-                String s = new String(buffer);
-                s = s.substring(read);
-
-                //on envoie le fichier voulu
-                try {
-                    FileInputStream file = new FileInputStream(s);
-                    while ((read = file.read(buffer)) != -1) {
-                        outToSend.write(buffer, 0, read);
-                    }
-                }catch (FileNotFoundException e){
-                    System.out.println(e.getMessage());
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }finally{
-                try {
-                    inToSend.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    outToSend.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    socketToSend.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
 
         }
     }
