fa701a57569976460c92dce2386f406b34a232d4
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-05-10 16:05:57 +0200

    _

--- odeep/src/main/java/message/MessageType.java
+++ odeep/src/main/java/message/MessageType.java
@@ -16,4 +16,5 @@ public class MessageType {
     public final static String UPLO = "UPLO"; //upload le fichier de config sur le serveur
     public final static String DISC = "DISC"; //indique que le destinataire d'un message est deconnecté
     public final static String NFIL = "NFIL"; //indique qu'on ne possede pas le fichier
+    public final static String NEWG = "NEWG"; //check si le nouveau groupe peut etre crée
 }
