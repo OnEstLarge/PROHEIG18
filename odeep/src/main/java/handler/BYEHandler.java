1d5e312025fe4f282b2c3483015df3e054c391e8
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-05-10 11:50:52 +0200

    modif handler

--- odeep/src/main/java/handler/BYEHandler.java
+++ odeep/src/main/java/handler/BYEHandler.java
@@ -6,6 +6,6 @@ import peer.PeerMessage;
 public class BYEHandler implements MessageHandler {
     @Override
     public void handleMessage(PeerConnection c, PeerMessage m) {
-
+        c.sendMessage(new PeerMessage(MessageType.UPDT, m.getIdGroup(),m.getIdTo(),m.getIdTo(),new byte[]{}));
     }
 }
