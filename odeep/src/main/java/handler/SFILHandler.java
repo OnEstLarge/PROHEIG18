ae7a770550fb5d549d06ad0b435e6584c3b71db6
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-05-11 10:39:07 +0200

    envoie de fichier

--- odeep/src/main/java/handler/SFILHandler.java
+++ odeep/src/main/java/handler/SFILHandler.java
@@ -1,51 +1,60 @@
 package handler;
 
+import Node.Node;
 import message.MessageHandler;
+import org.bouncycastle.crypto.InvalidCipherTextException;
 import peer.PeerConnection;
 import peer.PeerMessage;
 import util.CipherUtil;
 
+import javax.crypto.Cipher;
 import java.io.*;
 
-public class SFILHandler  implements MessageHandler {
+public class SFILHandler implements MessageHandler {
 
-    public void handleMessage(PeerConnection c, PeerMessage m){
-
-        byte[] rcv = CipherUtil.erasePadding(m.getMessageContent(), PeerMessage.PADDING_START);
-        if(m.getNoPacket() == 0){
-            String[] fileInfo = new String(rcv).split(":");
-            File file = new File("./shared_file/" + m.getIdGroup() + "/" + fileInfo[0]);
+    public void handleMessage(PeerConnection c, PeerMessage m) {
+        RandomAccessFile f = null;
+        byte[] key = null;
+        try {
+            f = new RandomAccessFile("./" + m.getIdGroup() + "/key", "r");
+            key = new byte[(int) f.length()];
+            f.readFully(key);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        int fileSize = Integer.parseInt(rcv[1]);
-        String filename = rcv[0];
-
-        System.out.println("Receiving "  + filename);
-
-        try {
-
-            InputStream is = c.getIs();
-            FileOutputStream fos = new FileOutputStream(filename);
-
-            int read = 0;
-            int totalRead = 0;
-            int remaining = fileSize;
-
-            byte[] buffer = new byte[4032];
-
-            while((read = is.read(buffer, 0, Math.min(buffer.length, remaining))) != -1 && remaining != 0){
-                //on ajuste la taille du content lu
-                //read -= PeerMessage.HEADER_SIZE;
-                totalRead += read;
-                remaining -= read;
-                //System.out.println("read " + totalRead + " bytes");
-                fos.write(buffer,0,read);//new PeerMessage(buffer).getMessageContent());
-                //buffer = new byte[Math.min(4032, remaining)];
+        byte[] rcv = CipherUtil.erasePadding(m.getMessageContent(), PeerMessage.PADDING_START);
+        if (m.getNoPacket() == 0) {
+            String[] fileInfo = new String(rcv).split(":");
+            Node.filesize = Integer.parseInt(fileInfo[1]);
+            Node.filename = fileInfo[0];
+            System.out.println("Receiving " + fileInfo[0]);
+            byte[] padding = new byte[PeerMessage.MESSAGE_CONTENT_SIZE];
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream("./shared_file/" + m.getIdGroup() + "/" + Node.filename);
+                for (int i = 0; i < (Node.filesize / PeerMessage.MESSAGE_CONTENT_SIZE) + 1; i++) {
+                    fos.write(padding);
+                }
+                fos.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
             }
-            fos.close();
-            System.out.println("Reception completed!");
-        } catch (FileNotFoundException e) {
-
-        } catch (IOException e) {}
+        } else {
+            try {
+                RandomAccessFile raf = new RandomAccessFile("./shared_file/" + m.getIdGroup() + "/" + Node.filename, "rw");
+                raf.seek(PeerMessage.MESSAGE_CONTENT_SIZE * (m.getNoPacket() - 1));
+                byte[] message = CipherUtil.erasePadding(m.getMessageContent(), PeerMessage.PADDING_START);
+                message = CipherUtil.AESDecrypt(message, key);
+                raf.write(message);
+                raf.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (InvalidCipherTextException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
