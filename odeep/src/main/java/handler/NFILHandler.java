f4952c9da45ddb0d5e339c73c27b30d18b58fa8a
loic-schurch <loic.schurch@heig-vd.ch>
2018-05-11 15:59:06 +0200

    client

--- odeep/src/main/java/handler/NFILHandler.java
+++ odeep/src/main/java/handler/NFILHandler.java
@@ -4,13 +4,13 @@ package handler;
 import message.MessageHandler;
 import peer.PeerConnection;
 import peer.PeerMessage;
-import util.DatabaseUtil;
+import main.Client;
 import util.InterfaceUtil;
 
 public class NFILHandler implements MessageHandler {
     @Override
     public void handleMessage(PeerConnection c, PeerMessage m) {
-        DatabaseUtil.downloadJSON(m.getIdGroup());
+        Client.downloadJSON(m.getIdGroup());
         InterfaceUtil.askFile(m.getIdGroup(), new String(m.getMessageContent()));
     }
 }
