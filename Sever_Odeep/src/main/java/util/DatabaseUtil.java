8c9907854939d652dbc777e4c0e588c041a56734
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-05-11 10:31:47 +0200

    Ajout méthode 'addUserIfNotExists()'

--- Sever_Odeep/src/main/java/util/DatabaseUtil.java
+++ Sever_Odeep/src/main/java/util/DatabaseUtil.java
@@ -6,7 +6,7 @@ public class DatabaseUtil {
 
     private static Connection connection;
 
-    private void initConnection() throws SQLException {
+    public void initConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/sakila?user=root&password=OdeepPRO2018";
         connection = DriverManager.getConnection(url);
     }
@@ -17,10 +17,9 @@ public class DatabaseUtil {
      * @param groupID   nom du groupe
      * @return 0,   le groupe est déjà présent dans la base de données ou erreur
      *         1,   le groupe est ajouté à la base de donnée
-     * @throws SQLException
      */
-    private static int addGroupIfNotExists(String groupID) throws SQLException {
-        String sql = "SELECT group_id FROM groups WHERE group_id = ?)";
+    public static int addGroupIfNotExists(String groupID) {
+        String sql = "SELECT group_id FROM groups WHERE group_id = ?;";
         ResultSet resultSet = null;
         int result = 0;
         PreparedStatement preparedStatement    = null;
@@ -32,7 +31,7 @@ public class DatabaseUtil {
             resultSet = preparedStatement.executeQuery();
 
             if(!resultSet.next()) {
-                String sqlAdd = "insert into groups(group_id) values(?)";
+                String sqlAdd = "INSERT INTO groups(group_id) VALUES(?);";
                 preparedStatementAdd = connection.prepareStatement(sqlAdd);
                 preparedStatementAdd.setString(1, groupID);
                 preparedStatementAdd.executeUpdate();
@@ -40,11 +39,59 @@ public class DatabaseUtil {
                 result = 1;
             }
 
+        } catch (SQLException e) {
+            e.printStackTrace();
         } finally {
-            preparedStatementAdd.close();
-            preparedStatement.close();
+            try {
+                preparedStatementAdd.close();
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
+        return result;
+    }
+
+    /**
+     * Ajoute un utilisateur dans la base de données s'il n'est pas déjà présent.
+     *
+     * @param username  nom d'utilisateur
+     * @return 0,       l'utilisateur est déjà présent dans la base de données ou erreur
+     *         1,       l'utilisateur est ajouté à la base de données
+     */
+    public static int addUserIfNotExists(String username) {
+        String sql = "SELECT username FROM users WHERE username = ?;";
+        ResultSet resultSet = null;
+        int result = 0;
+        PreparedStatement preparedStatement    = null;
+        PreparedStatement preparedStatementAdd = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+
+            if(!resultSet.next()) {
+                String sqlAdd = "INSERT INTO users(username) VALUES(?);";
+                preparedStatementAdd = connection.prepareStatement(sqlAdd);
+                preparedStatementAdd.setString(1, username);
+                preparedStatementAdd.executeUpdate();
+
+                result = 1;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                preparedStatementAdd.close();
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return result;
     }
 
